c0f0b74c4e76208c967ae4de4a21e05834f04ac5
imtiyaz-mulani <imtiyaz.mulani@gmail.com>
2018-07-01 20:51:08 +0530

    adding first time

---
+++ src/main/java/com/qa/sample/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.qa.sample;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld 
+{
+    @Test
+    public void PrintHelloWorld()
+    {
+    	System.out.println("Hello World");
+    }
+    
+    
+}

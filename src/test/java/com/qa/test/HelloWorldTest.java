c18af97097280ebb17bbdf1b0561d8f78b3d0e47
imtiyaz-mulani <imtiyaz.mulani@gmail.com>
2018-07-02 18:55:18 +0530

    added HelloWorldTest,java

---
+++ src/test/java/com/qa/test/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class HelloWorldTest 
+{
+	@Test
+    public void PrintHelloWorld()
+    {
+    	System.out.println("Hello World");
+    }
+
+}
